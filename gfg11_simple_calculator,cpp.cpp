2ac5d0359d080059c37c3ff482b6038f5aaa6ce2
anurag2u4u <147909064+anurag2u4u@users.noreply.github.com>
2023-10-14 13:28:05 +0530

    remove unused code

--- gfg11_simple_calculator,cpp.cpp
+++ gfg11_simple_calculator,cpp.cpp
@@ -6,18 +6,6 @@ int main(){
 	cin>>op;
 	cout<<"enter the values of x and y "<<endl;
 	cin>>x>>y;
-	switch(op){
-		case 1:
-			cout<<x+y;
-			break;
-		
-		case 2:
-			cout<<x-y;
-			break;
-			
-		case 3:
-		    cout<<x*y;
-			break;	
 		
 		default:
 		    cout<<"invalid operation"<<endl;	
